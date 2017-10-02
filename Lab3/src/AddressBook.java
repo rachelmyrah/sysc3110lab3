e8d08bf6902ca4eebd93976815add9543f11dbfe
rachelmyrah <rachelmyrah@cb5109-03.labs.sce.carleton.ca>
2017-10-02 09:06:29 -0400

    staged

---
+++ Lab3/src/AddressBook.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+	
+	private ArrayList<BuddyInfo> book;
+	
+	public AddressBook () {
+		book = new ArrayList<BuddyInfo>();
+	}
+	
+	public ArrayList<BuddyInfo> getAddressBook() {
+		return book;
+	}
+	
+	public void addBuddy(String name, String address, String phoneNumber) {
+		BuddyInfo buddy = new BuddyInfo(name, address, phoneNumber);
+		book.add(buddy);
+	}
+	
+	public void removeBuddy(String name) {
+		for (int i = 0; i < book.size(); i++) {
+			if (book.get(i).getName().equals(name)) {
+				book.remove(i);
+				break;
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		AddressBook addressBook = new AddressBook();
+		addressBook.addBuddy("Rachel", "123street", "123456789");
+		addressBook.removeBuddy("Rachel");
+	}
+}
---
+++ Lab3/src/BuddyInfo.java
@@ -0,0 +1,17 @@
+
+public class BuddyInfo {
+	
+	private String name;
+	private String address;
+	private String phoneNumber;
+
+	public BuddyInfo(String name, String address, String phoneNumber) {
+		this.name = name;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getName() {
+		return name;
+	}
+} 
